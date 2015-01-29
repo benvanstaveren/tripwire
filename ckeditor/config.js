ca6ba510dc4564488c95218e2517f59c7563b45d
Josh Glassmaker <josh.glassmaker@gmail.com>
2015-01-28 22:02:45 -0600

    Initial static files

---
+++ ckeditor/config.js
@@ -0,0 +1,60 @@
+/**
+ * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here.
+	// For complete reference see:
+	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
+
+	// The default plugins included in the basic setup define some buttons that
+	// are not needed in a basic editor. They are removed here.
+	//config.removeButtons = 'Cut,Copy,Paste,Undo,Redo,Anchor,Subscript,Superscript,Unlink';
+	/*
+	// Dialog windows are also simplified.
+	config.removeDialogTabs = 'link:advanced';
+
+	// Custom skin
+	config.skin = "custom";
+	config.height = 100;
+
+	//config.autoParagraph = false;
+	config.enterMode = CKEDITOR.ENTER_BR;
+	config.tabSpaces = 4;
+	config.allowedContent = true;
+
+	config.extraPlugins = 'toolbarswitch,autogrow';
+
+	config.autoGrow_onStartup = true;
+	config.autoGrow_minHeight = 100;
+
+	config.toolbar_minToolbar = [
+		{name: "basicstyles", items: ["Bold", "Italic", "Underline", "Strike"]},
+		{name: "paragraph", items: ["BulletedList", "Outdent", "Indent"]},
+		{name: "links", items: ["Link"]},
+		{name: "colors", items: ["TextColor", "BGColor"]},
+		{name: "styles", items: ["FontSize"]},
+		{name: "tools", items: ["Toolbarswitch"]}
+	];
+	
+	config.toolbar_maxToolbar = [
+		{name: "basicstyles", items: ["Bold", "Italic", "Underline", "Strike", "Subscript", "Superscript"]},
+		{name: "paragraph", items: ["NumberedList", "BulletedList", "Outdent", "Indent"]},
+		{name: "links", items: ["Link"]},
+		{name: "colors", items: ["TextColor", "BGColor"]},
+		{name: "styles", items: ["FontSize", "Font"]},
+		{name: "tools", items: ["Source", "Toolbarswitch"]}
+	];
+
+	config.toolbar = "minToolbar";
+	config.smallToolbar = "minToolbar";
+	config.maximizedToolbar = "maxToolbar";
+
+	config.fontSize_style = {
+	    element:        'span',
+	    styles:         { 'font-size': '#(size)' },
+	    overrides:      [ { element :'font', attributes: { 'size': null } } ]
+	};
+	*/
+};
