6d4ce5f2177c626155e65707a8b0ba86147adbb1
Josh Glassmaker <josh.glassmaker@gmail.com>
2015-08-12 19:52:34 -0500

    Replaced drag scroll plugin

--- tripwire.php
+++ tripwire.php
@@ -304,7 +304,7 @@ if ($row = $stmt->fetchObject()) {
 					<i id="show-favorite" data-icon="star" data-tooltip="Add favorite systems to chain"></i>
 					<i id="show-chainLegend" data-icon="tree" data-tooltip="<table id='guide'><tr><td><div class='guide-stable'></td><td>Stable</td></tr><tr><td><div class='guide-eol'></div></td><td>End of Life</td></tr><tr><td><div class='guide-destab'></div></td><td>Mass Destabbed</td></tr><tr><td><div class='guide-critical'></div></td><td>Mass Critical</td></tr><tr><td><div class='guide-frigate'></div></td><td>Frigate</td></tr></table>"></i>
 				</div>
-				<div id="chainParent" class="content">
+				<div id="chainParent" class="content dragscroll">
 					<span style="position: relative; display: table; width: 100%;">
 						<table id="chainGrid">
 							<tr class="top"><td></td></tr>
@@ -1152,7 +1152,7 @@ if ($row = $stmt->fetchObject()) {
 	<!-- JS Includes -->
 	<script type="text/javascript" src="//<?= $server ?>/js/combine.js"></script>
 	<script type="text/javascript" src="//<?= $server ?>/ckeditor/ckeditor.js"></script>
-	<script type="text/javascript" src="//<?= $server ?>/js/dragdivscroll.js"></script>
+	<script type="text/javascript" src="//<?= $server ?>/js/dragscroll.js"></script>
 	<script type="text/javascript" src="//<?= $server ?>/js/core.js"></script>
 	<!-- JS Includes -->
 
